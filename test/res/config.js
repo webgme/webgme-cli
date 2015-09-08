91d5b3442cddd039d31f09321fcb7f81a762a8c4
Brian Broll <brian.broll@vanderbilt.edu>
2015-09-07 21:31:41 -0500

    WIP #4. Added boilerplate for plugin test

---
+++ test/res/config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var path = require('path'),
+    config = require('webgme/config/config.default'),
+    validateConfig = require('webgme/config/validator');
+
+// The paths can be loaded from the webgme-setup.json
+config.plugin.basePaths.push(path.normalize(__dirname+"/../../src/commands/mixins/Enableable"));
+//config.addOn.basePaths.push("src/addOn");
+//config.seedProjects.basePaths.push("src/seed/OtherSeed");
+//config.seedProjects.basePaths.push("src/seed/alreadyExists");
+
+config.addOn.enable = true
+
+validateConfig(config);
+module.exports = config;
