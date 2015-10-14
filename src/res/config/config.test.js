dd04db043a5a10d98750ab57a74b1f4b1e8fc043
Hakan Tunc <hakan.u.tunc@outlook.com>
2015-10-14 12:12:46 -0500

    Fix #59

--- src/res/config/config.test.js
+++ src/res/config/config.test.js
@@ -3,7 +3,7 @@
  * @author lattmann / https://github.com/lattmann
  */
 
-var config = require('./config.webgme');
+var config = require('./config.default');
 
 config.server.port = 9001;
 config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_tests';
