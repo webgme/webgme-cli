b58d0244a0f61a50c8c65a0f7965a314b6f66df6
Brian Broll <brian.broll@vanderbilt.edu>
2016-04-29 08:17:09 -0500

    Declared `exportCli` Fixes #148

--- src/SeedManager.js
+++ src/SeedManager.js
@@ -35,7 +35,7 @@ SeedManager.prototype.new = function(args, callback) {
     // This is lazily loaded because the load takes a couple seconds. This
     // causes a delay for calling 'new' but no other commands
     if (!this._exportProject) {
-        exportCli = require('webgme/src/bin/export');
+        var exportCli = require('webgme/src/bin/export');
         this._exportProject = function (parameters, callback) {
             var args = [
                 'node', 'export.js',
